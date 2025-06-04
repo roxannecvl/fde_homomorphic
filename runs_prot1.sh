939ab16f013e0d14c9f77ac0ed9f62aaddcad8f9
Roxanne Chevalley <125833841+roxannecvl@users.noreply.github.com>
2025-06-04 11:09:02 +0200

    Update runs_prot1.sh

--- runs_prot1.sh
+++ runs_prot1.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Loop over each size and call ./script_eval.sh <size> three times
-for size in 256 512 768 1024; do
+for size in 128 256 512 768 1024; do
   echo "=== Running ./script_eval.sh $size (×3) ==="
   for i in {1..3}; do
     ./script_eval.sh "$size"
